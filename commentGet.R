bcacdc9bd6daf260929ad857b2ea50d809b44748
Sam <40672605+parmsam@users.noreply.github.com>
2020-10-31 13:09:32 -0400

    Add files via upload

--- commentGet.R
+++ commentGet.R
@@ -2,7 +2,7 @@ getCommentLines <- function(filename) {
   #read each code line using readLines()
   lines_vector <- readLines(filename, warn = FALSE) 
   comments_vector = c()
-  pattern = '#'
+  pattern = "#"
   for(codeline in lines_vector){
     if(grepl(pattern, codeline)){
       comments_vector = append(comments_vector, codeline)
@@ -35,3 +35,46 @@ getSpecialLines <- function(filename, pattern) {
   }
   return(comments_vector)
 }
+
+getTodoLines <- function(filename) {
+  #read each code line using readLines()
+  lines_vector <- readLines(filename, warn = FALSE) 
+  comments_vector = c()
+  pattern = paste(c("TODO","Todo", "todo"), collapse="|") 
+  for(codeline in lines_vector){
+    if(grepl(pattern, codeline)){
+      comments_vector = append(comments_vector, codeline)
+    }
+  }
+  return(comments_vector)
+}
+
+getFixmeLines <- function(filename) {
+  #read each code line using readLines()
+  lines_vector <- readLines(filename, warn = FALSE) 
+  comments_vector = c()
+  pattern = paste(c("FIXME","Fixme", "fixme", "Fix me", "fix me", "FIX ME"), collapse="|") 
+  for(codeline in lines_vector){
+    if(grepl(pattern, codeline)){
+      comments_vector = append(comments_vector, codeline)
+    }
+  }
+  return(comments_vector)
+}
+
+getNoteLines <- function(filename) {
+  #read each code line using readLines()
+  lines_vector <- readLines(filename, warn = FALSE) 
+  comments_vector = c()
+  pattern = paste(c("NOTE","Note", "note"), collapse="|")
+  for(codeline in lines_vector){
+    if(grepl(pattern, codeline)){
+      comments_vector = append(comments_vector, codeline)
+    }
+  }
+  return(comments_vector)
+}
+  
+#TODO: add copy to clipboard option in functions
+#TODO: add different output options
+#TODO: add support for other languages
