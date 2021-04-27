9fb3273da093d231e6e047c4c2b3bb8eee0340bb
Sam <40672605+parmsam@users.noreply.github.com>
2021-04-26 19:37:26 -0500

    updated functions

--- commentGet.R
+++ commentGet.R
@@ -1,14 +1,18 @@
+#load libraries
+library(utils)
+
+#file path of script in RStudio source editor
 getSourceEditorPath <- function() {
   #function to get path to file currently shown in source editor
-  path <- paste0(dirname(rstudioapi::getSourceEditorContext()$path),
-                 gsub("~","",rstudioapi::getSourceEditorContext()$path))
+  path <- paste0(rstudioapi::getSourceEditorContext()$path)
   return(path)
 }
 
-getSpecialLines <- function(
+#general function to retrieve lines of code based on pattern
+getLines <- function(
   filename=getSourceEditorPath(),
-  clipboard = TRUE,
-  output = TRUE,
+  clipboard,
+  output,
   pattern) {
   #function to get all lines with special pattern (specified) comment
   lines_vector <- readLines(filename, warn = FALSE)
@@ -27,49 +31,56 @@ getSpecialLines <- function(
   }
 }
 
+#apply general function with comment pattern
 getCommentLines <- function(
   filename=getSourceEditorPath(),
-  clipboard = TRUE, 
-  output = TRUE) {
+  clipboard = FALSE) {
   #function to get all lines with R comments
-  pattern = "#"
-  getSpecialLines(filename, clipboard, pattern, output)
+  pattern_string = "#"
+  getLines(filename, clipboard, pattern=pattern_string, output=TRUE)
 }
 
+#apply general function with outline pattern
 getOutlineLines <- function(
   filename=getSourceEditorPath(),
-  clipboard = TRUE,
-  output = TRUE) {
+  clipboard = FALSE) {
   #function to get all lines with outline comment
-  pattern = paste(c("####", "----"), collapse = "|")
-  getSpecialLines(filename, clipboard, pattern, output)
+  pattern_string = paste(c("####", "----"), collapse = "|")
+  getLines(filename, clipboard, pattern=pattern_string, output=TRUE)
 }
 
+#apply general function with TODO pattern
 getTodoLines <- function(
   filename=getSourceEditorPath(),
-  clipboard = TRUE,
-  output = TRUE) {
+  clipboard = FALSE) {
   #function to get all lines with TODO comment
-  pattern = paste(c("TODO","Todo", "todo"), collapse="|") 
-  getSpecialLines(filename, clipboard, pattern, output)
+  pattern_string = paste(c("TODO","Todo", "todo"), collapse="|") 
+  getLines(filename, clipboard, pattern=pattern_string, output=TRUE)
 }
 
+#apply general function with FIXME pattern
 getFixmeLines <- function(
   filename=getSourceEditorPath(),
-  clipboard = TRUE) {
+  clipboard = FALSE) {
   #function to get all lines with FIXME comment
-  pattern = paste(c("FIXME","Fixme", "fixme", "Fix me", "fix me", "FIX ME"), collapse="|") 
-  getSpecialLines(filename, clipboard, pattern, output)
+  pattern_string = paste(c("FIXME","Fixme", "fixme", "Fix me", "fix me", "FIX ME"), collapse="|") 
+  getLines(filename, clipboard, pattern=pattern_string, output=TRUE)
 }
 
+#apply general function with NOTE pattern
 getNoteLines <- function(
   filename=getSourceEditorPath(),
-  clipboard = TRUE,
-  output = TRUE) {
+  clipboard = FALSE) {
   #function to get all lines with NOTE comment
-  pattern = paste(c("NOTE","Note", "note"), collapse="|")
-  getSpecialLines(filename, clipboard, pattern, output)
+  pattern_string = paste(c("NOTE","Note", "note"), collapse="|")
+  getLines(filename, clipboard, pattern=pattern_string, output=TRUE)
 }
+
+#TODO: add line number list or column
+
+#TODO: convert to dataframe output
+# data.frame(Comment = unlist(getCommentLines() ))
+#TODO: add comments count function
 
 #TODO: add different output options
 #TODO: add support for other languages
